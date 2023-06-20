63ca7b34baabacc9375b46bca56ecb17437fe3dd
supuni001 <136358795+supuni001@users.noreply.github.com>
2023-06-20 18:54:18 +0530

    Update speaker.java

--- p32/src/main/java/com/mycompany/p32/speaker.java
+++ p32/src/main/java/com/mycompany/p32/speaker.java
@@ -1,13 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package com.mycompany.p32;
 
-/**
- *
- * @author Supuni
- */
-public interface speaker {
-    
+public interface speaker 
+{
+    void speak();
 }
